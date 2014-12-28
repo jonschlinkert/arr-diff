17cc24c6370d3b1eb0b3270acfa47213f2d8f74e
jonschlinkert <jon.schlinkert@sellside.com>
2014-12-28 05:33:52 -0500

    update test

--- test.js
+++ test.js
@@ -13,12 +13,29 @@ var diff = require('./');
 describe('diff', function () {
   it('should diff array:', function () {
     diff(['a', 'b', 'c'], ['b', 'c', 'e']).should.eql(['a']);
-    diff(['foo', 'b', 'c', 'e', 'bar'], ['b', 'foo', 'e']).should.eql(['bar', 'c']);
-    diff(['foo', 'foo'], ['a', 'b', 'c']).should.eql(['foo', 'foo']);
-    diff(['foo'], ['a', 'b', 'c']).should.eql([]);
+    diff(['x', 'b', 'c', 'e', 'y'], ['b', 'x', 'e']).should.eql(['c', 'y']);
+    diff(['x', 'x'], ['a', 'b', 'c']).should.eql(['x', 'x']);
+    diff(['x'], ['a', 'b', 'c']).should.eql(['x']);
+  });
+
+  it('should include duplicates:', function () {
+    diff(['x', 'b', 'b', 'b', 'c', 'e', 'y'], ['x', 'e']).should.eql(['b', 'b', 'b', 'c', 'y']);
+  });
+
+  it('should diff elements from multiple arrays:', function () {
+    diff(['a', 'b', 'c'], ['a'], ['b']).should.eql(['c']);
   });
 
   it('should return an empty array if no unique elements are in the first array:', function () {
-    diff(['foo'], ['a', 'b', 'c', 'foo']).should.eql([]);
+    diff(['a'], ['a', 'b', 'c']).should.eql([]);
+  });
+
+  it('should return the first array if the second array is empty:', function () {
+    diff(['a', 'b', 'c'], []).should.eql(['a', 'b', 'c']);
+  });
+
+  it('should return the first array if the second array is null or undefined:', function () {
+    diff(['a', 'b', 'c'], null).should.eql(['a', 'b', 'c']);
+    diff(['a', 'b', 'c']).should.eql(['a', 'b', 'c']);
   });
 });
